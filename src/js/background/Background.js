802be1604f32108e61e7993e49679f6dbe4e8cd6
Christophe Prome <cprome@viareport.com>
2015-02-18 16:08:19 +0100

    Actions on right click

--- src/js/background/Background.js
+++ src/js/background/Background.js
@@ -1,54 +1,59 @@
+var Actions = require('../Actions');
+var PreferencesService = require('../services/PreferencesService');
+
 module.exports = {
     init: function () {
-        chrome.runtime.onInstalled.addListener(function () {
-            var menuItems = [
-                {
-                    'title': chrome.i18n.getMessage('mettre_en_evidence'),
-                    'contexts': ['selection'],
-                    //'id': FilterModel.filterNames.highlighted,
-                    'onclick': addFilter
-                },
-                {
-                    'title': chrome.i18n.getMessage('rendre_discret'),
-                    'contexts': ['selection'],
-                    //'id': FilterModel.filterNames.muted,
-                    'onclick': addFilter
-                },
-                {
-                    'title': chrome.i18n.getMessage('masquer'),
-                    'contexts': ['selection'],
-                    //'id': FilterModel.filterNames.hidden,
-                    'onclick': addFilter
-                },
-                {
-                    'title': 'Séparateur',
-                    'contexts': ['selection'],
-                    'type': 'separator'
-                },
-                {
-                    'title': chrome.i18n.getMessage('reglages'),
-                    'contexts': ['all'],
-                    'onclick': displaySettingsMenu
-                },
-                {
-                    'title': 'Séparateur',
-                    'contexts': ['selection'],
-                    'type': 'separator'
-                },
-                {
-                    'title': chrome.i18n.getMessage('recherche_twitter'),
-                    'contexts': ['selection'],
-                    'onclick': searchOnTwitter
-                }
-            ];
+        PreferencesService.init().then(function() {
+            chrome.runtime.onInstalled.addListener(function () {
+                var menuItems = [
+                    {
+                        'title': chrome.i18n.getMessage('mettre_en_evidence'),
+                        'contexts': ['selection'],
+                        'id': 'highlighted',
+                        'onclick': addFilter
+                    },
+                    {
+                        'title': chrome.i18n.getMessage('rendre_discret'),
+                        'contexts': ['selection'],
+                        'id': 'muted',
+                        'onclick': addFilter
+                    },
+                    {
+                        'title': chrome.i18n.getMessage('masquer'),
+                        'contexts': ['selection'],
+                        'id': 'hidden',
+                        'onclick': addFilter
+                    },
+                    {
+                        'title': 'Séparateur',
+                        'contexts': ['selection'],
+                        'type': 'separator'
+                    },
+                    {
+                        'title': chrome.i18n.getMessage('reglages'),
+                        'contexts': ['all'],
+                        'onclick': displaySettingsMenu
+                    },
+                    {
+                        'title': 'Séparateur',
+                        'contexts': ['selection'],
+                        'type': 'separator'
+                    },
+                    {
+                        'title': chrome.i18n.getMessage('recherche_twitter'),
+                        'contexts': ['selection'],
+                        'onclick': searchOnTwitter
+                    }
+                ];
 
-            menuItems.forEach(function (menuItem) {
-                chrome.contextMenus.create(menuItem);
-            });
+                menuItems.forEach(function (menuItem) {
+                    chrome.contextMenus.create(menuItem);
+                });
 
-            execOnTwitterTabs(function (tab) {
-                chrome.tabs.executeScript(tab.id, {
-                    file: 'js/filters-bundle.js'
+                execOnTwitterTabs(function (tab) {
+                    chrome.tabs.executeScript(tab.id, {
+                        file: 'js/filters-bundle.js'
+                    });
                 });
             });
         });
@@ -72,10 +77,7 @@ function searchOnTwitter(info) {
 }
 
 function addFilter(info) {
-    var filter = info.selectionText.trim();
-    var filterType = info.menuItemId;
-
-    /*if (FilterStore.addFilter(filterType, filter)) {
-        Utils.updatePage();
-    }*/
+    var word = info.selectionText.trim();
+    var category = info.menuItemId;
+    Actions.createFilterWord(category,word);
 }
