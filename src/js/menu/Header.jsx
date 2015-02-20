40ea0b77fe585e2ca9d107777ac45326a604d43f
Christophe Promé <chprome@gmail.com>
2015-02-20 11:20:31 +0100

    Change text tweet button text

--- src/js/menu/Header.jsx
+++ src/js/menu/Header.jsx
@@ -9,7 +9,7 @@ var Header = React.createClass({
     render: function() {
         return (<div>
             <h1>QualiTweet
-                <a href="https://twitter.com/share?text=Enhance Twitter with QualiTweet!&url=http://goo.gl/K8ENYl&hashtags=twitter,chrome,extension,&via=Qualitiso_"
+                <a href="https://twitter.com/share?text=Filter Your Twitter Stream !&url=http://goo.gl/K8ENYl&hashtags=twitter,chrome,extension,&via=Qualitiso_"
                     target="_blank"
                     className="pull-right">
                     <img alt="Tweet" src="/images/btnTweet.png"/>
